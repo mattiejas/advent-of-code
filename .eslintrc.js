d1453c22bf6542e14cc8b585e5f40dd980c79fcf
Matthias Aarnoutse <mattiejas@outlook.com>
2019-12-06 23:39:13 +0100

    Solution 5

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: 'airbnb-base',
+  extends: ['airbnb-base', 'prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
