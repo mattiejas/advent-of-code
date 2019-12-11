900d684e1d87052c0055c2449eb0cf39b3d4b658
Matthias Aarnoutse <mattiejas@outlook.com>
2019-12-11 13:04:47 +0100

    Solve day 10 part 2

--- src/main/kotlin/y2019/d10/AsteroidField.kt
+++ src/main/kotlin/y2019/d10/AsteroidField.kt
@@ -1,4 +1,68 @@
-package y2019.`10`
+package y2019.d10
 
-class AsteroidField {
+import Vector
+
+class AsteroidField(data: List<String>) {
+    private var _map = mutableListOf<List<Boolean>>()
+    private var _amountInSight = mapOf<Vector<Int>, Int>()
+    private val _best: Vector<Int>
+
+    init {
+        _map = data.map { it.map { a -> a == '#' } }.toMutableList()
+        _best = findBestWatchtowerSpot()
+    }
+
+    fun amount(x: Int, y: Int): Int? {
+        return _amountInSight[Vector(x, y)]
+    }
+
+    fun watchtower(): Vector<Int> {
+        return _best
+    }
+
+    private fun asteroids(): List<Vector<Int>> =
+        _map.indices.flatMap { y -> _map[y].indices.map { x -> Vector(x, y) to _map[y][x] } }
+            .filter { p -> p.second }.map { it.first }
+
+    private fun findBestWatchtowerSpot(): Vector<Int> {
+        _amountInSight = asteroids()
+            .map { it to getAmountInSight(it) }
+            .toMap()
+
+        return _amountInSight.maxBy { it.value }!!.key
+    }
+
+    private fun getAmountInSight(p: Vector<Int>): Int {
+        val amount = asteroids().filter { it != p }
+            .map { it.angle(p) }
+            .distinct()
+
+        return amount.size
+    }
+
+    fun vaporize(): List<Vector<Int>> {
+        val angles = asteroids().groupBy { watchtower().angle(it) }
+            .map { it.key to it.value.sortedBy { it.magnitude() }.toMutableList() }
+            .sortedByDescending { it.first }
+            .toMutableList()
+
+        val vaporized = mutableListOf<Vector<Int>>()
+        var index = 0
+        while (angles.isNotEmpty()) {
+            val list = angles[index].second
+
+            if (list.isNotEmpty()) {
+                vaporized.add(list.removeAt(0))
+
+                if (list.isEmpty()) { // remove if it is empty
+                    angles.removeAt(index)
+                } else {
+                    index++
+                }
+
+                if (index >= angles.size) index = 0
+            }
+        }
+        return vaporized
+    }
 }
--- src/main/kotlin/y2019/d10/Day10.kt
+++ src/main/kotlin/y2019/d10/Day10.kt
@@ -1,77 +1,23 @@
-class Day10 : Day() {
+package y2019.d10
+
+import Day
+import Utils
+
+class Day10 : Day {
+    private val input = Utils().readFile("2019/10/input")
+    private val field = AsteroidField(input)
+
     override fun partOne() {
-        val input = Utils().readFile("input")
-        val a = AstroidField(this.input, 24, 24)
-        a.print()
-        println("${a.watchtower()}, amount: ${a.amount(a.watchtower().x.toInt(), a.watchtower().y.toInt())}")
-        a.printResult()
+        println(
+            "${field.watchtower()}, amount: ${field.amount(
+                field.watchtower().x,
+                field.watchtower().y
+            )}"
+        )
     }
 
     override fun partTwo() {
-        TODO("not implemented")
-    }
-
-    class AstroidField(val data: String, val width: Int, val height: Int) {
-        private val _map = mutableListOf<List<Boolean>>()
-        private var _amountInSight = mapOf<Vector<Int>, Int>()
-        private val _best: Vector<Int>
-
-        init {
-            val size = width
-            var count = 0
-            while ((count + 1) * size <= data.length) {
-                _map += data.substring(count * size, ++count * size)
-                    .toCharArray()
-                    .map { it == '#' }
-            }
-
-            _best = findBestWatchtowerSpot()
-        }
-
-        private fun hasAstroidAt(x: Int, y: Int): Boolean {
-            return _map[y][x]
-        }
-
-        fun amount(x: Int, y: Int): Int? {
-            return _amountInSight[Vector(x, y)]
-        }
-
-        fun watchtower(): Vector<Int> {
-            return _best
-        }
-
-        private fun asteroids(): List<Vector<Int>> =
-            _map.indices.flatMap { y -> _map[y].indices.map { x -> Vector(x, y) to _map[y][x] } }
-                .filter { p -> p.second }.map { it.first }
-
-        private fun findBestWatchtowerSpot(): Vector<Int> {
-            _amountInSight = asteroids()
-                .map { it to getAmountInSight(it) }
-                .toMap()
-
-            return _amountInSight.maxBy { it.value }!!.key
-        }
-
-        private fun getAmountInSight(p: Vector<Int>): Int {
-            val amount = asteroids().filter { it != p }
-                .map { it.angle(p) }
-                .distinct()
-
-            return amount.size
-        }
-
-        fun print() {
-            data.chunked(width)
-                .map { it.map { it.toString().padStart(2, ' ') }.reduce { acc, s -> "$acc  $s" } }
-                .forEach { println(it) }
-        }
-
-        fun printResult() {
-            (0 until height).flatMap { y -> (0 until width).map { x -> Vector(x, y) } }
-                .map { p -> p to (_amountInSight[p] ?: '.') }
-                .chunked(width)
-                .map { it.map { it.second.toString().padStart(2, ' ') }.reduce { acc, s -> "$acc  $s" } }
-                .forEach { println(it) }
-        }
+        val a200 = field.vaporize()[199]
+        println(a200)
     }
 }
