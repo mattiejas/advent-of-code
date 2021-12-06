25a3a9c200676f8df600ab3bee04a18467a9e643
Matthias Aarnoutse <matthias@bitlag.studio>
2021-12-06 16:19:49 +0100

    Implement day 02 in Rust

--- y2021-rs/src/main.rs
+++ y2021-rs/src/main.rs
@@ -1,13 +1,13 @@
 use crate::day::Day;
 
 mod day;
-mod day01;
+mod day02;
 mod utils;
 
 fn main() {
     let now = std::time::Instant::now();
 
-    println!("{}", day01::Day01::solve_part2());
+    println!("{}", day02::Day02::solve_part2());
 
     println!(
         "ran for {}ms",
