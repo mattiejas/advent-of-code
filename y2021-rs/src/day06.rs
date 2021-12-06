6fc7b2746d229a47ad81776bd505d62ad28b1c8c
Matthias Aarnoutse <matthias@bitlag.studio>
2021-12-07 00:47:22 +0100

    optimize day 6 into oblivion

--- y2021-rs/src/day06.rs
+++ y2021-rs/src/day06.rs
@@ -3,35 +3,26 @@ use crate::utils;
 
 pub struct Day06 {}
 
-fn parse_input(input: &str) -> Vec<u8> {
-    input
-        .split('\n')
-        .next()
-        .expect("No input")
+fn get_initial_fish(input: &str) -> Vec<usize> {
+    let initial_fish: Vec<u8> = input
+        .trim()
         .split(',')
-        .filter(|s| !s.is_empty())
-        .map(|x| x.parse::<u8>())
+        .map(|x| x.to_owned().parse::<u8>())
         .flatten()
-        .collect()
-}
-
-fn simulate(days: u32) -> String {
-    let input = utils::input(6, false);
-    let initial_fish = parse_input(&input);
-
-    let mut fish: Vec<usize> = (0..=8)
-        .into_iter()
-        .map(|key| {
-            initial_fish
-                .iter()
-                .filter(|x| *x.clone() as usize == key)
-                .count()
-        })
         .collect();
 
-    // simulate days
+    let mut fish: Vec<usize> = vec![0; 9]; // move fish into age brackets
+    for x in initial_fish {
+        fish[x as usize] += 1;
+    }
+    return fish;
+}
+
+fn simulate(days: u16) -> String {
+    let mut fish = get_initial_fish(&utils::input(6, false)); // comma separated fish
+
     for _ in 0..days {
-        fish.rotate_left(1);
+        fish.rotate_left(1); // simulate days
         fish[6] += fish[8];
     }
 
