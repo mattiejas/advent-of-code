727f70e4a9562df0b372b04499ff9d65a3f89b39
Matthias Aarnoutse <matthias@bitlag.studio>
2021-12-09 22:28:47 +0100

    Implement part 1 of day 9

--- y2021-rs/src/day09.rs
+++ y2021-rs/src/day09.rs
@@ -3,70 +3,84 @@ use crate::{day, utils};
 pub struct Day09 {}
 
 fn get_heightmap() -> Vec<Vec<i32>> {
-    let input = utils::input(9, true);
+    let input = utils::input(9, false);
     return input
         .split('\n')
         .filter(|x| !x.is_empty())
-        .map(|x| x.chars().map(|y| y.to_digit(10).unwrap() as i32).collect())
+        .map(|x| {
+            x.trim()
+                .chars()
+                .map(|y| y.to_digit(10).unwrap() as i32)
+                .collect()
+        })
         .collect();
 }
 
-fn get_gradient(heightmap: &Vec<Vec<i32>>, x: usize, y: usize) -> f32 {
-    let height = heightmap[y][x];
-    let nx = heightmap[0].len() - 1;
-    let ny = heightmap.len() - 1;
+// check if point is local minima
+fn is_local_minima(heightmap: &Vec<Vec<i32>>, x: usize, y: usize) -> bool {
+    let mut local_minima = true;
 
-    let mut dx = 0.;
-    let mut dy = 0.;
-
-    match x {
-        0 => {
-            dx += (heightmap[y][x + 1] - height) as f32;
+    match (x, y) {
+        (0, 0) => {
+            local_minima =
+                heightmap[y][x] < heightmap[y][x + 1] && heightmap[y][x] < heightmap[y + 1][x];
         }
-        nx => {
-            dx += (heightmap[y][x - 1] - height) as f32;
+        (0, _) => {
+            if y == heightmap.len() - 1 {
+                local_minima =
+                    heightmap[y][x] < heightmap[y][x + 1] && heightmap[y][x] < heightmap[y - 1][x];
+            } else {
+                local_minima = heightmap[y][x] < heightmap[y][x + 1]
+                    && heightmap[y][x] < heightmap[y + 1][x]
+                    && heightmap[y][x] < heightmap[y - 1][x];
+            }
         }
-        _ => {
-            dx += (heightmap[y][x - 1] - height) as f32;
-            dx += (heightmap[y][x + 1] - height) as f32;
-            dx /= 2.;
+        (_, 0) => {
+            if x == heightmap[y].len() - 1 {
+                local_minima =
+                    heightmap[y][x] < heightmap[y + 1][x] && heightmap[y][x] < heightmap[y][x - 1];
+            } else {
+                local_minima = heightmap[y][x] < heightmap[y][x - 1]
+                    && heightmap[y][x] < heightmap[y][x + 1]
+                    && heightmap[y][x] < heightmap[y + 1][x];
+            }
+        }
+        (_, _) => {
+            if x == heightmap[y].len() - 1 && y == heightmap.len() - 1 {
+                local_minima =
+                    heightmap[y][x] < heightmap[y - 1][x] && heightmap[y][x] < heightmap[y][x - 1];
+            } else if x == heightmap.len() - 1 {
+                local_minima = heightmap[y][x] < heightmap[y - 1][x]
+                    && heightmap[y][x] < heightmap[y + 1][x]
+                    && heightmap[y][x] < heightmap[y][x - 1];
+            } else if y == heightmap.len() - 1 {
+                local_minima = heightmap[y][x] < heightmap[y - 1][x]
+                    && heightmap[y][x] < heightmap[y][x + 1]
+                    && heightmap[y][x] < heightmap[y][x - 1];
+            } else {
+                local_minima = heightmap[y][x] < heightmap[y - 1][x]
+                    && heightmap[y][x] < heightmap[y + 1][x]
+                    && heightmap[y][x] < heightmap[y][x - 1]
+                    && heightmap[y][x] < heightmap[y][x + 1];
+            }
         }
     }
 
-    match y {
-        0 => {
-            dy += (heightmap[y + 1][x] - height) as f32;
-        }
-        Some(n) if n == y => {
-            dy += (heightmap[y - 1][x] - height) as f32;
-        }
-        _ => {
-            dy += (heightmap[y - 1][x] - height) as f32;
-            dy += (heightmap[y + 1][x] - height) as f32;
-            dy /= 2.;
-        }
-    }
-
-    return (dx.powi(2) + dy.powi(2)).sqrt();
-}
-
-fn get_gradients(heightmap: &Vec<Vec<i32>>) -> Vec<Vec<f32>> {
-    let mut gradients = vec![vec![0.; heightmap[0].len()]; heightmap.len()];
-
-    for y in 0..heightmap.len() {
-        for x in 0..heightmap[0].len() {
-            gradients[y][x] = get_gradient(heightmap, x, y);
-        }
-    }
-
-    gradients
+    return local_minima;
 }
 
 impl day::Day for Day09 {
     fn solve_part1() -> String {
-        let gradients = get_gradients(&get_heightmap());
-        println!("{:#?}", gradients);
-        return "".to_string();
+        let heightmap = get_heightmap();
+        let mut risk_factors = vec![];
+        for y in 0..heightmap.len() {
+            for x in 0..heightmap[y].len() {
+                if is_local_minima(&heightmap, x, y) {
+                    risk_factors.push(heightmap[y][x] + 1);
+                }
+            }
+        }
+        return risk_factors.iter().sum::<i32>().to_string();
     }
 
     fn solve_part2() -> String {
